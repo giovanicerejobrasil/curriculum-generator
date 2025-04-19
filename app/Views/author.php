4ae64374bf211869ca3047ad04ce5b0b75da1e80
Giovani Brasil <giovanicerejobrasil@gmail.com>
2025-04-18 21:24:49 -0300

    adjust author view

--- app/Views/author.php
+++ app/Views/author.php
@@ -1,7 +1,7 @@
 <?php require_once __DIR__ . "/partials/header.php"; ?>
-        
-<main class="w-full flex flex-10 flex-col items-center bg-(--white) py-5">
-    
+
+<main class="w-full flex flex-col items-center bg-(--white) py-5">
+  <h1 class="text-6xl">Coming Soon...</h1>
 </main>
 
 <?php require_once __DIR__ . "/partials/footer.php"; ?>
