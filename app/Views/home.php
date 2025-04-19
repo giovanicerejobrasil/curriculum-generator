e2e51ccb381d5ed3e1ee7a756c75a42b42432984
Giovani Brasil <giovanicerejobrasil@gmail.com>
2025-04-18 21:23:27 -0300

    adjust home text

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,23 +1,20 @@
 <?php require_once __DIR__ . "/partials/header.php"; ?>
 
 <main class="flex flex-col items-center bg-(--white) py-5">
-    <article class="text-justify px-25 my-5">
+    <article class="w-[50%] text-justify my-5">
         <h1 class="text-center text-6xl font-bold my-8">Curriculum Generator</h1>
-        <p class="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil voluptates dignissimos magnam illum, possimus porro repellendus ratione consequatur cumque maxime iusto tempore distinctio eveniet explicabo delectus voluptatem pariatur repellat praesentium?
-        Qui quos veniam ipsum voluptates. Porro, eos! Dicta nemo optio obcaecati quae iure repellat facilis! Doloremque at alias, natus hic, aspernatur ea repellendus molestias, et ipsa atque ipsam quis vitae?
-        Doloremque, est et expedita omnis adipisci quam eaque, dolorum iusto eum officiis deserunt reprehenderit? A magni facilis aperiam commodi sequi culpa voluptas rerum voluptates nulla corporis, aspernatur rem explicabo provident!
-        Voluptas, minima provident accusamus temporibus qui debitis ad non sequi. Soluta totam assumenda veritatis recusandae iste deleniti amet nisi quibusdam in harum? Omnis suscipit fugit delectus voluptates facere in facilis!
-        Praesentium libero illum est ea dolores ullam, doloremque, corporis ipsam consequatur explicabo consequuntur commodi? Quia culpa deserunt maxime vel voluptates? Optio voluptas aut veritatis cumque fuga itaque unde perferendis omnis?
-        Pariatur nobis aperiam sequi fuga alias. Quibusdam similique recusandae esse odio quod, commodi ab aut? Vero saepe ut nihil! Magni, iure illum. Tempore libero iure facere consectetur ea quod et.
-        Harum, at deleniti cum impedit perspiciatis neque a assumenda perferendis. Hic quasi harum maiores amet. Nihil aliquam modi voluptas expedita, aliquid eos praesentium magnam error voluptatibus, fugiat asperiores magni consequatur.
-        Officia quasi atque deserunt labore repellendus laborum aliquid reprehenderit numquam magnam suscipit, aperiam, assumenda ipsa minus praesentium tempora aliquam delectus ipsam nemo amet harum dolore itaque reiciendis voluptatem. Animi, error?
-        Perspiciatis inventore officiis eius ullam corrupti porro magni quam necessitatibus eos rem ut in soluta commodi adipisci dicta consequatur, esse non maiores. A, nemo. Laboriosam nostrum suscipit autem quisquam facilis?
-        Sunt, voluptates? Saepe, quod vitae. Nostrum aspernatur exercitationem deleniti quae reprehenderit dolorem quis, alias voluptatem, error quisquam asperiores fuga, pariatur facere? Minima dicta quis eos quibusdam atque hic beatae voluptas?</p>
+        <p class="text-lg mb-5">
+            This project was created to generate a custom curriculum information for a specifc job vacancy for you used the perfect keywords and experiences for the vacancy. Of course, this project is not a magic, but it can help you to generate a curriculum with a good quality. Enjoy it!
+        </p>
+
+        <p class="text-lg">
+            This project initially just generete a curriculum in brazilian portuguese. But, in the future, I will add more languages, and you can selected the language you want to use.
+        </p>
     </article>
 
-    <a 
-    href="/generate"
-    class="my-5 px-4 py-2 text-2xl opacity-80 flex justify-center items-center rounded-lg bg-(--thistle) text-(--black) transition-all delay-50 duration-150 ease-in-out hover:bg-(--french-rose) hover:text-(--white) hover:opacity-100 shadow-lg">
+    <a
+        href="/generate"
+        class="my-5 px-4 py-2 text-2xl opacity-80 flex justify-center items-center rounded-lg bg-(--thistle) text-(--black) transition-all delay-50 duration-150 ease-in-out hover:bg-(--french-rose) hover:text-(--white) hover:opacity-100 shadow-lg">
         <span class="material-symbols-outlined">settings</span>
         <span>Generate</span>
     </a>
