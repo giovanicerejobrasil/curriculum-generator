f953e0851db1bbe9b53f775fef7198b6be8e38ff
Giovani Brasil <giovanicerejobrasil@gmail.com>
2025-04-22 00:15:10 -0300

    add link of this project on github

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 define("PROJECT_NAME", "Curriculum Generator");
+define("PROJECT_LINK_GITHUB", "https://github.com/giovanicerejobrasil/curriculum-generator");
 define('BASE_URL', 'http://localhost:8080');
 define('FILES_DIR', [
   'templates',
