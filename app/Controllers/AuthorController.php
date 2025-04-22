140dabb1b605c582ebb802138757580be61da0c0
Giovani Brasil <giovanicerejobrasil@gmail.com>
2025-04-22 00:16:25 -0300

    adjust of the data provided for author view

--- app/Controllers/AuthorController.php
+++ app/Controllers/AuthorController.php
@@ -8,7 +8,14 @@ class AuthorController extends Controller
 {
   public function index()
   {
-    $data['title'] =  PROJECT_NAME . " - Autor";
+    $data = [
+      "title" => PROJECT_NAME . " - Autor",
+      "project" => (object) [
+        "name" => PROJECT_NAME,
+        "link" => PROJECT_LINK_GITHUB
+      ]
+    ];
+
     return $this->loadView("author", $data);
   }
 }
